ee0eb5257e316171aaa0f9bdd7272fd70c3f1abd
noah <noah@iamnoah.com>
2017-03-13 01:57:19 -0700

    default alpha

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const parse = require('parse-color');
 
 const DEFAULT_COLOR = 'rgba(0, 0, 0, 0.5)';
+const DEFAULT_ALPHA = 0.5;
 
-function makeTransparent(color) {
+function makeTransparent(color, alpha = DEFAULT_ALPHA) {
   if (!color) return DEFAULT_COLOR;
 
   const { rgb } = parse(color);
 
   if (!rgb) return color;
 
-  return `rgba(${rgb.join(', ')}, 0.5)`;
+  return `rgba(${rgb.join(', ')}, ${alpha})`;
 }
 
 module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
