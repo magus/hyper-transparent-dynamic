2e67a32d19847157e31821f26623c09973a61a61
noah <noah@iamnoah.com>
2017-03-13 01:56:10 -0700

    dynamic background color

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const parse = require('parse-color');
+
+const DEFAULT_COLOR = 'rgba(0, 0, 0, 0.5)';
+
+function makeTransparent(color) {
+  if (!color) return DEFAULT_COLOR;
+
+  const { rgb } = parse(color);
+
+  if (!rgb) return color;
+
+  return `rgba(${rgb.join(', ')}, 0.5)`;
+}
+
 module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
 
 module.exports.decorateConfig = config => {
-  config.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-  return config;
-}
+  return Object.assign({}, config, {
+    backgroundColor: makeTransparent(config.backgroundColor),
+  });
+};
