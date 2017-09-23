96c26f9a3b19cd0c1592e1179d4271d4956b1956
Levin Rickert <me@levrik.io>
2017-09-23 13:14:38 +0200

    Added Windows 10 support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const parse = require('parse-color');
+const os = require('os');
 
 const CONFIG_KEY = 'hyperTransparentDynamic';
 const DEFAULT_COLOR = 'rgba(0, 0, 0, 0.5)';
@@ -14,7 +15,14 @@ function makeTransparent(color, alpha = DEFAULT_ALPHA) {
   return `rgba(${rgb.join(', ')}, ${alpha})`;
 }
 
-module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
+module.exports.onWindow = browserWindow => {
+  if (process.platform === 'win32' && os.release().startsWith('10.')) {
+    const vibrancy = require('electron-vibrancy');
+    vibrancy.SetVibrancy(browserWindow);
+  } else {
+    browserWindow.setVibrancy('dark');
+  }
+};
 
 module.exports.decorateConfig = config => {
   const { alpha } = config[CONFIG_KEY] || {};
